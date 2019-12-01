7bbee4fa8b97957f6fc565785eb8817f67a2bd8c
hammad1985 <hammad.yaqoob1985@yahoo.co.uk>
2019-12-01 15:48:37 +0000

    Added routing and forms

--- react/authorquiz/src/AuthorQuiz.js
+++ react/authorquiz/src/AuthorQuiz.js
@@ -54,9 +54,17 @@ Turn.propTypes = {
   onAnswerSelected: PropTypes.func.isRequired
 };
 
-function Continue() {
-  return (<div />);
-}
+function Continue({show, onContinue}) {
+  return ( 
+  <div className= "row continue">
+  {
+    show ? 
+    <div className="col-11">
+    <button className="btn btn-primary btn-lg float-right" onClick={onContinue}>Continue</button>
+    </div> : null
+    
+  }</div>)
+  }
 
 function Footer() {
   return (<div id="footer" className="row"> 
@@ -68,12 +76,12 @@ function Footer() {
   </div>);
 }
 
-function AuthorQuiz({turnData, highlight, onAnswerSelected}) {
+function AuthorQuiz({turnData, highlight, onAnswerSelected, onContinue}) {
   return (
     <div className="container-fluid">
       <Hero />
       <Turn {...turnData} highlight={highlight} onAnswerSelected={onAnswerSelected}/>
-      <Continue />
+      <Continue show={highlight === 'correct'} onContinue={onContinue}/>
       <p><Link to="/add">Add an Author</Link></p>
       <Footer />
     </div>
--- react/authorquiz/src/index.js
+++ react/authorquiz/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, withRouter} from 'react-router-dom';
 import './index.css';
 import AuthorQuiz from './AuthorQuiz';
+import AddAuthorForm from './AddAuthorForm';
 import * as serviceWorker from './serviceWorker';
 import {shuffle, sample} from 'underscore';
 
@@ -76,10 +77,15 @@ function getTurnData(authors) {
         author.books.some((title) => title === answer))
     }
 }
-const state = {
-    turnData : getTurnData(authors),
-    highlight : ''
-};
+
+function resetState() {
+    return {
+        turnData : getTurnData(authors),
+        highlight : ''
+    };
+}
+
+let state = resetState();
 
 function onAnswerSelected(answer) {
     const isCorrect = state.turnData.author.books.some((book) => book === answer);
@@ -87,25 +93,29 @@ function onAnswerSelected(answer) {
     render();
 }
 
-function AddAuthorForm({match}) {
-    return <div>
-        <h1>
-            Add Author
-        </h1>
-        <p>
-            {JSON.stringify(match)}
-        </p>
-    </div>
-}
 function App() {
-    return <AuthorQuiz {...state} onAnswerSelected={onAnswerSelected}/>;
+    return <AuthorQuiz {...state} 
+    onAnswerSelected={onAnswerSelected}
+    onContinue={() => {
+        state = resetState();
+        render();
+    }
+    }/>;
 }
+
+const AddAuthorWrapper = withRouter(({history}) =>
+    <AddAuthorForm onAddAuthor={(author) => {
+        authors.push(author);
+        history.push("/");
+    }}/>
+);
+
 function render() {
     ReactDOM.render(
     <BrowserRouter>
     <React.Fragment>
         <Route exact path="/" component= {App} />
-        <Route path="/add" component= {AddAuthorForm} />
+        <Route path="/add" component= {AddAuthorWrapper} />
     </React.Fragment>
     </BrowserRouter>, document.getElementById('root'));
 }
