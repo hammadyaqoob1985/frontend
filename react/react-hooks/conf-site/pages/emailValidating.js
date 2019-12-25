4118e85a1b7aae6eb5686bf11f00ab33cf308425
hammad1985 <hammad.yaqoob1985@yahoo.co.uk>
2019-12-25 15:38:43 +0000

    created email validating page

---
+++ react/react-hooks/conf-site/pages/emailValidating.js
@@ -0,0 +1,9 @@
+import React from "react";
+import EmailValidatingForm from "../src/EmailValidatingForm";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function emailValidating() {
+    return <EmailValidatingForm/>;
+}
+
+export default emailValidating;
